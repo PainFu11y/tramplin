d209867bcf6118c17a82329326ae4ffb63bc8e5f
Painfu11y <333vahan777@gmail.com>
2026-03-22 20:40:39 +0400

    feat: ResourceNotFoundException

---
+++ src/main/java/com/tramplin/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.tramplin.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
