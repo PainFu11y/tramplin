56dcc61780384a631d0208200a9218869dc1f573
Painfu11y <333vahan777@gmail.com>
2026-03-22 20:39:51 +0400

    feat: repository functions

--- src/main/java/com/tramplin/repository/CompanyVerificationRepository.java
+++ src/main/java/com/tramplin/repository/CompanyVerificationRepository.java
@@ -1,7 +1,13 @@
 package com.tramplin.repository;
 
 import com.tramplin.entity.CompanyVerification;
+import com.tramplin.enums.VerificationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-public interface CompanyVerificationRepository extends JpaRepository<CompanyVerification, UUID> {}
+public interface CompanyVerificationRepository extends JpaRepository<CompanyVerification, UUID> {
+    List<CompanyVerification> findByStatus(VerificationStatus status);
+    Optional<CompanyVerification> findByCompanyId(UUID companyId);
+}
--- src/main/java/com/tramplin/repository/EmployerRepository.java
+++ src/main/java/com/tramplin/repository/EmployerRepository.java
@@ -2,6 +2,9 @@ package com.tramplin.repository;
 
 import com.tramplin.entity.Employer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 import java.util.UUID;
 
-public interface EmployerRepository extends JpaRepository<Employer, UUID> {}
+public interface EmployerRepository extends JpaRepository<Employer, UUID> {
+    Optional<Employer> findByUserId(UUID userId);
+}
