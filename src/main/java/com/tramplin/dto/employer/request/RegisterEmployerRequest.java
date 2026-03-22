d675ab24f80f01ddd91b37ee4c3be45d297b0538
Painfu11y <333vahan777@gmail.com>
2026-03-22 20:14:52 +0400

    feat: register dto's

---
+++ src/main/java/com/tramplin/dto/employer/request/RegisterEmployerRequest.java
@@ -0,0 +1,27 @@
+package com.tramplin.dto.employer.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class RegisterEmployerRequest {
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+
+    @NotBlank(message = "Company name is required")
+    private String companyName;
+
+    @NotNull(message = "Tax number is required")
+    private Integer taxNumber;
+
+    private String websiteUrl;
+}
